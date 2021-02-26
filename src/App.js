9c7dac1d40af8299fb6d2403a0ffd52f692a251c
Ben Thompson <btomo86@gmail.com>
2021-02-26 11:01:37 +0000

    added Navigation component and added some basic styles using tachyons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+import "tachyons";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
