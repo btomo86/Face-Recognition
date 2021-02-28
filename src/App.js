814e51a479ea9b0127ec79e949c8a7a43b916b34
Ben Thompson <btomo86@gmail.com>
2021-02-28 15:47:38 +0000

    added sign in form with routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Particles from "react-particles-js";
 import { Component } from "react";
 import Clarifai from "clarifai";
 import FaceRecognition from "./components/FaceRecognition";
+import Signin from "./components/Signin";
 
 const app = new Clarifai.App({
   apiKey: "41388ef8975248b9960297b6e2f0cace",
@@ -32,6 +33,7 @@ class App extends Component {
       input: ",",
       imageUrl: "",
       box: {},
+      route: "signin",
     };
   }
 
@@ -68,18 +70,31 @@ class App extends Component {
       .catch((err) => console.log(err));
   };
 
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+  };
+
   render() {
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onSubmit={this.onSubmit}
-        />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Navigation onRouteChange={this.onRouteChange} />
+        {this.state.route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onSubmit={this.onSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        )}
       </div>
     );
   }
